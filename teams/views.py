9cc4d9758aed8237bbb6ad44a94b37f413843984
Diego <dieguinhorj05@gmail.com>
2024-04-26 20:51:56 -0300

    feat: tarefa 3 finalizada

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, status
+from rest_framework.response import Response
+from django.forms.models import model_to_dict
+from .models import Team
+from utils import (
+    data_processing,
+    ImpossibleTitlesError,
+    InvalidYearCupError,
+    NegativeTitlesError,
+)
 
-# Create your views here.
+
+class TeamsView(APIView):
+    def post(self, request):
+        team_data = request.data
+
+        try:
+            data_processing(team_data)
+        except (
+            NegativeTitlesError,
+            ImpossibleTitlesError,
+            InvalidYearCupError,
+        ) as error:
+            return Response({"error": error.message}, status.HTTP_400_BAD_REQUEST)
+
+        team = Team.objects.create(**team_data)
+        team_dict = model_to_dict(team)
+
+        return Response(team_dict, status.HTTP_201_CREATED)
+
+    def get(self, request):
+        teams = Team.objects.all()
+        team_dict = []
+
+        for team in teams:
+            t = model_to_dict(team)
+            team_dict.append(t)
+
+        return Response(team_dict)
--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ def data_processing(data):
     if data["titles"] < 0:
         raise NegativeTitlesError("titles cannot be negative")
 
-    if int(first_cup) % 4 != 2:
+    if int(first_cup) % 4 != 2 or int(first_cup) < 1930:
         raise InvalidYearCupError("there was no world cup this year")
 
     if data["titles"] > num_copas:
